ecf881eff166f3c9b9d9b928a3e26b76e9c5ec2e
AlanOP03 <perxalan18@gmail.com>
2025-02-10 02:16:12 +0100

    Maui2 Parte 2

--- Maui_app/MainPage.xaml.cs
+++ Maui_app/MainPage.xaml.cs
@@ -1,38 +1,46 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using System.Diagnostics;
 
 namespace MauiCode;
 
 public partial class MainPage : ContentPage
 {
-    Button loginButton;
-    VerticalStackLayout layout;
-
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
     public MainPage()
     {
-        this.BackgroundColor = Color.FromArgb("512bdf");
+        InitializeComponent();
 
-        layout = new VerticalStackLayout
+        if (File.Exists(_fileName))
         {
-            Margin = new Thickness(15, 15, 15, 15),
+            editor.Text = File.ReadAllText(_fileName);
+        }
+        var notesHeading = new Label() { Text = "Notes", HorizontalOptions = LayoutOptions.Center, FontAttributes = FontAttributes.Bold };
+
+        editor = new Editor() { Placeholder = "Enter your note", HeightRequest = 100 };
+        editor.Text = File.Exists(_fileName) ? File.ReadAllText(_fileName) : string.Empty;
+
+        var buttonsGrid = new Grid() { HeightRequest = 40.0 };
+        buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1.0, GridUnitType.Auto) });
+        buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(30.0, GridUnitType.Absolute) });
+        buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1.0, GridUnitType.Auto) });
+
+        var saveButton = new Button() { WidthRequest = 100, Text = "Save" };
+        saveButton.Clicked += OnSaveButtonClicked;
+        Grid.SetColumn(saveButton, 0);
+        buttonsGrid.Children.Add(saveButton);
+
+        var deleteButton = new Button() { WidthRequest = 100, Text = "Delete" };
+        deleteButton.Clicked += OnDeleteButtonClicked;
+        Grid.SetColumn(deleteButton, 2);
+        buttonsGrid.Children.Add(deleteButton);
+
+        var stackLayout = new VerticalStackLayout
+        {
             Padding = new Thickness(30, 60, 30, 30),
-            Children =
-            {
-                new Label { Text = "Please log in", FontSize = 30, TextColor = Color.FromRgb(255, 255, 100) },
-                new Label { Text = "Username", TextColor = Color.FromRgb(255, 255, 255) },
-                new Entry (),
-                new Label { Text = "Password", TextColor = Color.FromRgb(255, 255, 255) },
-                new Entry { IsPassword = true }
-            }
+            Children = { notesHeading, editor, buttonsGrid }
         };
 
-        loginButton = new Button { Text = "Login", BackgroundColor = Color.FromRgb(0, 148, 255) };
-        layout.Children.Add(loginButton);
-
-        Content = layout;
-
-        loginButton.Clicked += (sender, e) =>
-        {
-            Debug.WriteLine("Clicked !");
-        };
+        this.Content = stackLayout;
     }
 }
