51eeba3e80cc42dddfccc11c4cdc5c87b4d89d8e
AlanOP03 <perxalan18@gmail.com>
2025-02-17 13:35:01 +0100

    Ejercicio Base para MAUI Ejercicio 9

---
+++ Maui_app9/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui_app9
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Maui_app9/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui_app9
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
