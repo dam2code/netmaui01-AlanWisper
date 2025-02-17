0e511cafd2c9a9043ea0c2402b1895942aad10a0
AlanOP03 <perxalan18@gmail.com>
2025-02-17 12:42:36 +0100

    Base Para hacer Ejercicio Maui7

---
+++ Maui_app7/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui_app7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Maui_app7/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Maui_app7
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
